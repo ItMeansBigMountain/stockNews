b1a688f24824d0b4fde2b7c9a0e14dfe9d68b1ca
itMeansBigMountain <affan.fareed@gmail.com>
2024-02-22 11:34:04 -0600

    CORS

--- stock_news_backend/stock_news_backend/settings.py
+++ stock_news_backend/stock_news_backend/settings.py
@@ -25,14 +25,23 @@ SECRET_KEY = 'django-insecure-^xv#j2h=)!3jg3ycq&p)l59+dshk3f%c#^5vj%313ef)wq+ouc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = []
+
+# CORS
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:4200', 
+# ]
+
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "core",
     "rest_framework",
+    'corsheaders',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +51,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
