7fddce532f8a280796172e9db84195d1416baad8
itMeansBigMountain <affan.fareed@gmail.com>
2024-02-21 19:46:40 -0600

    JWT generate & authenticate user token for all endpoints

--- stock_news_backend/core/urls.py
+++ stock_news_backend/core/urls.py
@@ -1,16 +1,15 @@
 from rest_framework.routers import DefaultRouter
-from .views import StockViewSet, NewsSourceViewSet
+from . import views
 
 from django.urls import path, include
 from django.http import HttpResponse 
-from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 
 
 router = DefaultRouter()
-router.register(r'stocks', StockViewSet)
-router.register(r'newssources', NewsSourceViewSet)
+router.register(r'stocks', views.StockViewSet)
+router.register(r'newssources', views.NewsSourceViewSet)
 
 urlpatterns = [
     path("", lambda request: HttpResponse("Welcome!")),
@@ -21,6 +20,9 @@ urlpatterns = [
     # JWT AUTH
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/users/', views.CreateUserView.as_view(), name='create_user'),
+
 ]
 
 
--- stock_news_backend/core/views.py
+++ stock_news_backend/core/views.py
@@ -1,11 +1,36 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import CustomUser, Stock, NewsSource
 from .serializers import UserSerializer, StockSerializer, NewsSourceSerializer
+from django.contrib.auth.hashers import make_password
 
+# ViewSet for Stocks
 class StockViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]  
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
 
+# ViewSet for News Sources
 class NewsSourceViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]  
     queryset = NewsSource.objects.all()
     serializer_class = NewsSourceSerializer
+
+# API View for creating a new user
+class CreateUserView(APIView):
+    permission_classes = [AllowAny] 
+
+    def post(self, request):
+        data = request.data
+        
+        # Hash the password
+        data['password'] = make_password(request.data.get('password'))  
+        
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
